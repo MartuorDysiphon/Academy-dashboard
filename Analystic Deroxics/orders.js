055c97ca6588e55a75bf6aea23c85a040a14d5e3
MartuorDysiphon <129260867+MartuorDysiphon@users.noreply.github.com>
2023-08-13 14:50:48 +0200

    Add files via upload

---
+++ Analystic Deroxics/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Enterprise BlockChain',
+        productNumber: '654685743',
+        paymentStatus: 'Paid',
+        status: 'In Session'
+    },
+    {
+        productName: 'Python Full Course',
+        productNumber: '569756245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Aitificia Intelligence Tutorial',
+        productNumber: '675836452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
